f757b27e4b0fbf2b008eb80ab9f979801ecd3547
marcospaulorgomes <marcospaulorgomes.dev@gmail.com>
2024-06-03 20:56:39 -0300

    marcos.gomes: Alteração do código sql utilizado no banco

--- books.sql
+++ books.sql
@@ -1,62 +1,60 @@
--- ####Código de criação das tabelas no banco#### --
+-- #### Criação das tabelas #### --
 
 -- Cria a Tabela Livro para armazenar as informações sobre os livros.
 CREATE TABLE Livro (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    isbn VARCHAR(13) UNIQUE NOT NULL,
+    isbn VARCHAR(13) PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
     autor VARCHAR(255) NOT NULL,
     categoria VARCHAR(255) NOT NULL,
-    dataPublicacao DATE NOT NULL,
+    data_publicacao DATE NOT NULL,
     descricao TEXT,
-    numPaginas INT CHECK (numPaginas >= 0),
-    link_Thumbnail VARCHAR(255),
-    notaMedia FLOAT CHECK (notaMedia BETWEEN 1 AND 5),
+    num_paginas INT CHECK (num_paginas >= 0),
+    link_thumbnail VARCHAR(255),
+    nota_media FLOAT CHECK (nota_media BETWEEN 1 AND 5)
 );
 
 -- Cria a Tabela Review para armazenar as informações sobre as reviews.
 CREATE TABLE Review (
-    id INT PRIMARY KEY AUTO_INCREMENT,
     autor VARCHAR(255) NOT NULL,
-    dataPublicacao DATE NOT NULL,
+    livro_isbn VARCHAR(13) NOT NULL,
+    data_publicacao DATE NOT NULL,
     sumario TEXT,
-    link_URL_Review VARCHAR(255),
-    livroId INT NOT NULL,
-    FOREIGN KEY (livroId) REFERENCES Livro(id)
+    link_url_review VARCHAR(255),
+    PRIMARY KEY (autor, livro_isbn),
+    FOREIGN KEY (livro_isbn) REFERENCES Livro(isbn)
 );
 
 -- Cria a Tabela Lista para armazenar as informações sobre as listas.
 CREATE TABLE Lista (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    dataPublicacao DATE NOT NULL,
-    dataAvaliacao DATE,
+    nome VARCHAR(255) PRIMARY KEY,
+    data_publicacao DATE NOT NULL,
+    data_avaliacao DATE
 );
 
 -- Cria a Tabela Livrosdalista para representar a relação entre Livro e Lista.
 CREATE TABLE Livrosdalista (
-    listaId INT NOT NULL,
-    livroId INT NOT NULL,
-    rank INT NOT NULL CHECK (rank BETWEEN 1 AND 15),
-    PRIMARY KEY (listaId, livroId),
-    FOREIGN KEY (listaId) REFERENCES Lista(id),
-    FOREIGN KEY (livroId) REFERENCES Livro(id)
+    lista_nome VARCHAR(255) NOT NULL,
+    livro_isbn VARCHAR(13) NOT NULL,
+    rank INT CHECK (rank BETWEEN 1 AND 15),
+    PRIMARY KEY (lista_nome, livro_isbn),
+    FOREIGN KEY (lista_nome) REFERENCES Lista(nome),
+    FOREIGN KEY (livro_isbn) REFERENCES Livro(isbn)
 );
 
--- #### Criação de indices #### --
+-- #### Criação de índices #### --
 
--- Cria o indice autor do livro
+-- Cria o índice autor do livro
 CREATE INDEX idx_autor ON Livro (autor);
 
--- Cria o indice categoria/data
-CREATE INDEX idx_categoria_data ON Livro (categoria, dataPublicacao);
+-- Cria o índice categoria/data
+CREATE INDEX idx_categoria_data ON Livro (categoria, data_publicacao);
 
--- Cria o indice nome da lista
+-- Cria o índice nome da lista
 CREATE INDEX idx_nome ON Lista (nome);
 
 -- #### Criação dos usuários do banco #### --
 
---Cria o usuário DBA
+-- Cria o usuário DBA
 CREATE USER user_dba WITH PASSWORD 'senha1234';
 ALTER USER user_dba WITH SUPERUSER;
 
@@ -64,4 +62,3 @@ ALTER USER user_dba WITH SUPERUSER;
 CREATE USER user_app WITH PASSWORD '321senha';
 GRANT USAGE ON SCHEMA books TO user_app;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA books TO user_app;
-
