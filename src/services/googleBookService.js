3d62b3b73272fc564ee8da73213fc234b0af70f9
souzaisa <souza.isa96@gmail.com>
2024-07-04 18:25:25 -0300

    Atualizando importações

--- src/services/googleBookService.js
+++ src/services/googleBookService.js
@@ -1,12 +1,11 @@
 import fetch from 'node-fetch';
 
-const API_KEY_GB = 'AIzaSyBL8ms47_xUZbStZDQJAt7dGpkeuGPe1aw';
 async function searchBooksBySubjects(subject) {
   let startIndex = 500;
   let books = [];
   for (let i = 0; i < 60; i++) {
     try {
-      const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=subject=' + subject + '&orderBy=relevance&printType=books&maxResults=40&startIndex=' + startIndex + '&key=' + API_KEY_GB);
+      const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=subject=' + subject + '&orderBy=relevance&printType=books&maxResults=40&startIndex=' + startIndex);
       const responseJSON = await response.json();
       books.push(responseJSON);
     } catch (erro) {
